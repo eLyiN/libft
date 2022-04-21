a175e2289195e9215c77aedc045abc5404a99586
Adrian Arribas lopera <aarribas@c1r3s4.42malaga.com>
2022-04-21 20:35:07 +0200

    add new ft's

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: aarribas <aarribas@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 19:11:12 by aarribas          #+#    #+#             */
-/*   Updated: 2022/04/21 19:18:09 by aarribas         ###   ########.fr       */
+/*   Updated: 2022/04/21 20:22:11 by aarribas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void    *ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char *dest_temp;
-	char *src_temp;
+	unsigned char *dest_temp;
+	unsigned char *src_temp;
 	size_t i;
 
 	i = 0;
-	dest_temp = (char *)dest;
-	src_temp = (char *)src;
+	dest_temp = (unsigned char *)dest;
+	src_temp = (unsigned char *)src;
 
 	while (i < n)
 	{
--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -1,4 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarribas <aarribas@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 20:22:47 by aarribas          #+#    #+#             */
+/*   Updated: 2022/04/21 20:34:15 by aarribas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
 void *ft_memmove(void *dest, const void *src, size_t n)
 {
+	int i;
 
+	i = (int *)n-1;
+	if (dest > src)
+	{
+		while (i--)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char*)src)[i];
+		}
+	}
+	else
+	{
+		ft_memcpy(dest, src, n);
+	}
+	return (dest);
 }
