ca28322516d4bad4161fbb8b2b1de691015db9e0
Adrian Arribas lopera <aarribas@c2r10s4.42malaga.com>
2022-04-23 18:43:52 +0200

    path move + new adds

---
+++ ft_strlcpy.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarribas <aarribas@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/23 16:44:47 by aarribas          #+#    #+#             */
+/*   Updated: 2022/04/23 17:59:11 by aarribas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (src[j])
+	{
+		j++;
+	}
+	if (size < 1)
+		return (j);
+	else
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+	}
+	dst[i] = '\0';
+	return (j);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+    char destino[] = "s";
+    char source[] = "";
+
+    printf("Esto es mi funcion: %ld\n", ft_strlcpy(destino, source, 9));
+    printf("Esto es mi funcion el dest: %s\n", destino);
+
+    printf("Esta es la original: %ld\n", strlcpy(destino, source, 9));
+    printf("Esta es la original y el dest: %s\n", destino);
+ 
+}*/
---
+++ ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarribas <aarribas@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/23 16:47:24 by aarribas          #+#    #+#             */
+/*   Updated: 2022/04/23 17:56:46 by aarribas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+int	main(void)
+{
+	int	i;
+
+	i = ft_strlen("asdasdasdasd");
+	return (0);
+}
